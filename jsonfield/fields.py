603e305de7e0f9a415f3a070c928d433611a40ce
Lauréline Guérin <zebuline@crocobox.org>
2011-05-20 15:10:02 +0200

    add date/datetime support

--- jsonfield/fields.py
+++ jsonfield/fields.py
@@ -1,12 +1,72 @@
+from decimal import Decimal
+import datetime
+
 from django.db import models
-from django.core.serializers.json import DjangoJSONEncoder
+from django.conf import settings
 from django.utils import simplejson as json
 
 try:
-	import cPickle as pickle
+    import cPickle as pickle
 except ImportError:
-	import pickle
+    import pickle
 
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return str(obj)
+        elif isinstance(obj, (datetime.date, datetime.datetime, datetime.time)):
+            return obj.isoformat()
+        return super(JSONEncoder, self).default(obj)
+
+
+def datetime_decoder(d):
+    if isinstance(d, list):
+        pairs = enumerate(d)
+    elif isinstance(d, dict):
+        pairs = d.items()
+    result = []
+    for k, v in pairs:
+        if isinstance(v, basestring):
+            try:
+                v = datetime.datetime.strptime(v, '%Y-%m-%dT%H:%M:%S')
+            except ValueError, e:
+                try:
+                    v = datetime.datetime.strptime(v, '%Y-%m-%d').date()
+                except ValueError:
+                    pass
+        elif isinstance(v, (dict, list)):
+            v = datetime_decoder(v)
+        result.append((k, v))
+    if isinstance(d, list):
+        return [x[1] for x in result]
+    elif isinstance(d, dict):
+        return dict(result)
+
+
+def dumps(value):
+    assert isinstance(value, dict)
+    return JSONEncoder().encode(value)
+
+
+def loads(txt):
+    value = json.loads(
+        txt,
+        object_hook=datetime_decoder,
+        parse_float=Decimal,
+        encoding=settings.DEFAULT_CHARSET
+    )
+    assert isinstance(value, dict)
+    return value
+
+
+class JSONDict(dict):
+    """
+    Hack so repr() called by dumpdata will output JSON instead of
+    Python formatted data. This way fixtures will work!
+    """
+    def __repr__(self):
+        return dumps(self)
 
 
 class JSONField(models.TextField):
@@ -24,7 +84,8 @@ class JSONField(models.TextField):
 
         try:
             if isinstance(value, basestring):
-                return json.loads(value)
+                res = loads(value)
+                return JSONDict(**res)
         except ValueError:
             pass
 
@@ -37,46 +98,56 @@ class JSONField(models.TextField):
             return None
 
         if isinstance(value, (dict, list)):
-            value = json.dumps(value, cls=DjangoJSONEncoder)
+            value = dumps(value)
 
         return super(JSONField, self).get_db_prep_save(value, connection=connection)
 
+    def south_field_triple(self):
+        """Returns a suitable description of this field for South."""
+        # We'll just introspect the _actual_ field.
+        from south.modelsinspector import introspector
+        field_class = "django.db.models.fields.TextField"
+        args, kwargs = introspector(self)
+        # That's our definition!
+        return (field_class, args, kwargs)
+
 
 class PickledObject(str):
-	"""A subclass of string so it can be told whether a string is
-	   a pickled object or not (if the object is an instance of this class
-	   then it must [well, should] be a pickled one)."""
-	pass
+    """A subclass of string so it can be told whether a string is
+       a pickled object or not (if the object is an instance of this class
+       then it must [well, should] be a pickled one)."""
+    pass
 
 class PickledObjectField(models.Field):
-	__metaclass__ = models.SubfieldBase
-	
-	def to_python(self, value):
-		if isinstance(value, PickledObject):
-			# If the value is a definite pickle; and an error is raised in de-pickling
-			# it should be allowed to propogate.
-			return pickle.loads(str(value))
-		else:
-			try:
-				return pickle.loads(str(value))
-			except:
-				# If an error was raised, just return the plain value
-				return value
-	
-	def get_db_prep_save(self, value, connection):
-		if value is not None and not isinstance(value, PickledObject):
-			value = PickledObject(pickle.dumps(value))
-		return value
-	
-	def get_internal_type(self): 
-		return 'TextField'
-	
-	def get_db_prep_lookup(self, lookup_type, value, connection, prepared=False):
-		if lookup_type == 'exact':
-			value = self.get_db_prep_save(value)
-			return super(PickledObjectField, self).get_db_prep_lookup(lookup_type, value, connection, prepared)
-		elif lookup_type == 'in':
-			value = [self.get_db_prep_save(v) for v in value]
-			return super(PickledObjectField, self).get_db_prep_lookup(lookup_type, value, connection, prepared)
-		else:
-			raise TypeError('Lookup type %s is not supported.' % lookup_type)
+    __metaclass__ = models.SubfieldBase
+
+    def to_python(self, value):
+        if isinstance(value, PickledObject):
+            # If the value is a definite pickle; and an error is raised in de-pickling
+            # it should be allowed to propogate.
+            return pickle.loads(str(value))
+        else:
+            try:
+                return pickle.loads(str(value))
+            except:
+                # If an error was raised, just return the plain value
+                return value
+
+    def get_db_prep_save(self, value, connection):
+        if value is not None and not isinstance(value, PickledObject):
+            value = PickledObject(pickle.dumps(value))
+        return value
+
+    def get_internal_type(self):
+        return 'TextField'
+
+    def get_db_prep_lookup(self, lookup_type, value, connection, prepared=False):
+        if lookup_type == 'exact':
+            value = self.get_db_prep_save(value)
+            return super(PickledObjectField, self).get_db_prep_lookup(lookup_type, value, connection, prepared)
+        elif lookup_type == 'in':
+            value = [self.get_db_prep_save(v) for v in value]
+            return super(PickledObjectField, self).get_db_prep_lookup(lookup_type, value, connection, prepared)
+        else:
+            raise TypeError('Lookup type %s is not supported.' % lookup_type)
+
